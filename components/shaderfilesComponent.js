189aa94060cc69d2c9f16b606af08e239eff4d4c
dirkk0 <dirkk0@googlemail.com>
2017-02-19 12:33:27 +0100

    changed file handling

--- components/shaderfilesComponent.js
+++ components/shaderfilesComponent.js
@@ -11,20 +11,20 @@
 
          // kudos Davide Aversa
          // http://www.davideaversa.it/2016/10/three-js-shader-loading-external-file/
-         function ShaderLoader(mesh, vertex_url, fragment_url, onLoad, onProgress, onError) {
-           console.log(mesh);
+         function ShaderLoader(mesh, vertex_url, fragment_url, texture0_url, onLoad, onProgress, onError) {
+           // console.log(mesh);
              var vertex_loader = new THREE.FileLoader(THREE.DefaultLoadingManager);
              vertex_loader.setResponseType('text');
              vertex_loader.load(vertex_url, function (vertex_text) {
                  var fragment_loader = new THREE.FileLoader(THREE.DefaultLoadingManager);
                  fragment_loader.setResponseType('text');
                  fragment_loader.load(fragment_url, function (fragment_text) {
-                     onLoad(mesh, vertex_text, fragment_text);
+                     onLoad(mesh, vertex_text, fragment_text, texture0_url);
                  });
              }, onProgress, onError);
          }
 
-         var myOnload = function(mesh, fs, vs) {
+         var myOnload = function(mesh, fs, vs, texture0_url) {
 
             var uniforms = {
                 iGlobalTime: { type: "f", value: 1.0 },
@@ -37,10 +37,25 @@
             uniforms.iResolution.value.y = 0.05; // window.innerHeight;
 
 
+
+
+uniforms1 = {
+	time:       { value: 1.0 },
+	resolution: { value: new THREE.Vector2(1.0,1.0) },
+};
+
+uniforms2 = {
+  time:       { value: 1.0 },
+  resolution: { value: new THREE.Vector2(1.0,1.0) },
+  texture0:    { value: new THREE.TextureLoader().load( texture0_url ) }
+};
+
+uniforms2.texture0.value.wrapS = uniforms2.texture0.value.wrapT = THREE.RepeatWrapping;
+
             mesh.material = new THREE.ShaderMaterial({
               vertexShader: vs,
              	fragmentShader: fs,
-              uniforms: uniforms
+              uniforms: uniforms2
             });
 
             // console.log(mesh.material.uniforms);
@@ -49,8 +64,9 @@
 
          ShaderLoader(
            thisMesh,
-           this.data.filename+".fragment.glsl",
-           this.data.filename+".vertex.glsl",
+           this.data.filename+".fragment.glsl?a=" + Math.random(),
+           this.data.filename+".vertex.glsl?a=" + Math.random(),
+           this.data.filename+"_0.jpg?a=" + Math.random(),
            myOnload
          );
 
@@ -61,7 +77,8 @@
         var thisMesh = this.el.object3D.children[0];
 
         if (thisMesh.material.type == "ShaderMaterial") {
-          thisMesh.material.uniforms.iGlobalTime.value = time/1000;
+          // thisMesh.material.uniforms.iGlobalTime.value = time/1000;
+          thisMesh.material.uniforms.time.value = time/1000;
         }
 
         // var obj = aaa.getObject3D('mesh');
--- shaders/4.fragment.glsl
+++ shaders/4.fragment.glsl
@@ -4,19 +4,19 @@ uniform float iGlobalTime;
 uniform vec2 iResolution;
 
 
-            varying vec2 vUv;
-            varying vec2 fragCoord;
+varying vec2 vUv;
+varying vec2 fragCoord;
 
-            void main( void ) {
+void main( void ) {
 
-                float time = iGlobalTime;
-                vec2 resolution = iResolution;
+    float time = iGlobalTime;
+    vec2 resolution = iResolution;
 
-                vec2 position = -1.0 + 2.0 * vUv;
+    vec2 position = -1.0 + 2.0 * vUv;
 
-                float red = abs( sin( position.x * position.y + time / 5.0 ) );
-                float green = abs( sin( position.x * position.y + time / 4.0 ) );
-                float blue = abs( sin( position.x * position.y + time / 3.0 ) );
-                gl_FragColor = vec4( red, green, blue, 1.0 );
+    float red = abs( sin( position.x * position.y + time / 5.0 ) );
+    float green = abs( sin( position.x * position.y + time / 4.0 ) );
+    float blue = abs( sin( position.x * position.y + time / 3.0 ) );
+    gl_FragColor = vec4( red, green, blue, 1.0 );
 
-            }
+}
